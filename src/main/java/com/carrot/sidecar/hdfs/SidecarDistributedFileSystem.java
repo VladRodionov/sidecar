230431ee6202b036b2cc9dbaea62d212ef1f825a
Vladimir Rodionov <vladrodionov@gmail.com>
2023-01-16 17:24:08 -0800

    Add HDFS support #4

---
+++ src/main/java/com/carrot/sidecar/hdfs/SidecarDistributedFileSystem.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.carrot.sidecar.hdfs;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.EnumSet;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CreateFlag;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.util.Progressable;
+
+import com.carrot.sidecar.SidecarCachingFileSystem;
+
+public class SidecarDistributedFileSystem extends DistributedFileSystem {
+  private SidecarCachingFileSystem sidecar;
+  
+  public SidecarDistributedFileSystem() {}
+  
+  @Override
+  public void initialize(URI name, Configuration originalConf) throws IOException {
+    super.initialize(name, originalConf);
+    this.sidecar = SidecarCachingFileSystem.get(this);
+    this.sidecar.initialize(name, originalConf);
+  }
+
+  @Override
+  public FSDataInputStream open(Path f, int bufferSize) throws IOException {
+    return sidecar.open(f, bufferSize);
+  }
+
+  @Override
+  public FSDataOutputStream create(Path f, FsPermission permission, boolean overwrite,
+      int bufferSize, short replication, long blockSize, Progressable progress) throws IOException {
+    return sidecar.create(f, permission, overwrite, bufferSize, replication, blockSize, progress);
+  }
+
+  @Override
+  public FSDataOutputStream createNonRecursive(Path path, FsPermission permission,
+      EnumSet<CreateFlag> flags, int bufferSize, short replication, long blockSize,
+      Progressable progress) throws IOException {
+    return sidecar.createNonRecursive(path, permission, flags, bufferSize, replication, blockSize,
+      progress);
+  }
+
+  @Override
+  public FSDataOutputStream append(Path f, int bufferSize, Progressable progress)
+      throws IOException {
+    return sidecar.append(f, bufferSize, progress);
+  }
+
+  @Override
+  public boolean rename(Path src, Path dst) throws IOException {
+    return sidecar.rename(src, dst);
+  }
+
+  @Override
+  public boolean delete(Path f, boolean recursive) throws IOException {
+    return sidecar.delete(f, recursive);
+  }
+
+  @Override
+  public void close() throws IOException {
+    super.close();
+    sidecar.close();
+  }
+  
+
+}
