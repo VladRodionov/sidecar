19d938a37bc097647dd28ffff8344b2451c2674d
Vladimir Rodionov <vladrodionov@gmail.com>
2023-02-02 16:29:10 -0800

    Add Open Stack Swift support #40

--- src/main/java/com/carrot/sidecar/swift/SidecarSwiftNativeFileSystem.java
+++ src/main/java/com/carrot/sidecar/swift/SidecarSwiftNativeFileSystem.java
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.carrot.sidecar.oss;
+package com.carrot.sidecar.swift;
 
 import java.io.IOException;
 import java.net.URI;
@@ -26,10 +9,11 @@ import org.apache.hadoop.fs.CreateFlag;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileAlreadyExistsException;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.Options.Rename;
-import org.apache.hadoop.fs.aliyun.oss.AliyunOSSFileSystem;
 import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.fs.swift.snative.SwiftNativeFileSystem;
 import org.apache.hadoop.util.Progressable;
 
 import com.carrot.sidecar.CachingFileSystem;
@@ -37,16 +21,19 @@ import com.carrot.sidecar.MetaDataCacheable;
 import com.carrot.sidecar.SidecarCachingFileSystem;
 
 /**
+ * Implementation of {@link FileSystem} for <a href="https://www.openstack.org">
+ * Swift Object Store</a>, used to access Swift system in a file-system style.
  * 
- * Sidecar caching FS for some Chineese Aliyun OSS (Alibaba Cloud)
- * fs.oss.impl=com.carrot.sidecar.oss.SidecarAliyunOSSFileSystem
- */
-public class SidecarAliyunOSSFileSystem extends AliyunOSSFileSystem 
-implements MetaDataCacheable, CachingFileSystem {
+ * fs.swift.impl=com.carrot.sidecar.swift.SidecarSwiftNativeFileSystem
 
+ */
+
+public class SidecarSwiftNativeFileSystem extends SwiftNativeFileSystem 
+  implements MetaDataCacheable, CachingFileSystem
+{
   private SidecarCachingFileSystem sidecar;
   
-  public SidecarAliyunOSSFileSystem() {}
+  public SidecarSwiftNativeFileSystem() {}
   
   @Override
   public void initialize(URI name, Configuration originalConf) throws IOException {
